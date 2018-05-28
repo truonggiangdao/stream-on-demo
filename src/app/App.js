5739f35480c24bf229b7dfe7ae72a47ded35adee
Giang Dao <giangdao@success-ss.com.vn>
2018-05-28 11:31:20 +0700

    add Home Container

--- src/app/App.js
+++ src/app/App.js
@@ -1,49 +1,20 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
-import { User } from './components/User';
-import { Main } from './components/Main';
 import Login from './containers/Login';
-import EditProfile from './containers/EditProfile';
-import { setName } from './userActions';
+import Home from './containers/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Main changeUsername={() => this.props.setName("Anna")}/>
-        <User username={this.props.user.name}/>
-
-        <BrowserRouter>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/profile">Edit Profile</Link>
-
-            <Switch>
-              <Route path="/login" component={Login}/>
-              <Route path="/profile" component={EditProfile}/>
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-    math: state.math
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setName: (name) => {
-      dispatch(setName(name));
-    }
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
