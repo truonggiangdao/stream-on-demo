4d5c98f294a887a4308ed6657303e54c5bfd5b35
Giang Dao <giangdao@success-ss.com.vn>
2018-05-28 16:08:17 +0700

    update test

--- src/app/containers/MainLayout/MainLayout.test.js
+++ src/app/containers/MainLayout/MainLayout.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainLayout from './MainLayout';
+import { BrowserRouter } from 'react-router-dom';
 
 it('MainLayout Component renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MainLayout />, div);
+  ReactDOM.render((
+    <BrowserRouter>
+      <MainLayout match={{path: ''}} />
+    </BrowserRouter>
+  ), div);
   ReactDOM.unmountComponentAtNode(div);
 });
