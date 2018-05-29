85796a78757e8680aa75bc58350cd0a78237cea7
truonggiangdao <daotruonggiang91@gmail.com>
2018-05-29 22:31:59 +0700

    focus input field when click on label

--- src/app/components/InputField/InputField.js
+++ src/app/components/InputField/InputField.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './InputField.scss';
 
-const InputField = (props) => (
-  <div className="group-input-field">
-    <label className="input-field-label" htmlFor={props.name}>
-      {props.label}
-      { props.required && (
-        <span className="input-field-required">*</span>
-      )}:
-    </label>
-    <input className="input-field" type={props.type} name={props.name} />
-  </div>
-);
+class InputField extends Component {
+  constructor(props) {
+    super(props);
+    this.inputEl = React.createRef();
+  }
+
+  handleLabelClick() {
+    this.inputEl.current.focus();
+  }
+
+  render() {
+    return (
+      <div className="group-input-field">
+        <label className="input-field-label" htmlFor={this.props.name} onClick={() => this.handleLabelClick()}>
+          {this.props.label || ''}
+          { this.props.required && (
+            <span className="input-field-required">*</span>
+          )}:
+        </label>
+        <input
+          className="input-field"
+          ref={this.inputEl}
+          type={this.props.type}
+          name={this.props.name}
+          placeholder={this.props.label || ''} />
+      </div>
+    );
+  }
+};
 
 export default InputField;
