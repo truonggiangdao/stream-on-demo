a70af598a62c7cbb0b127e7a56603dfc48fbc558
Giang Dao <giangdao@success-ss.com.vn>
2018-05-30 11:51:28 +0700

    clean reducers

--- src/app/store/reducers.js
+++ src/app/store/reducers.js
@@ -1,75 +1,47 @@
 import { combineReducers } from 'redux';
 
-const mathReducer = (state = {
-    result: 1,
-    lastValues: []
-}, action) => {
-    switch (action.type) {
-        case "ADD":
-            state = {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        case "SUBTRACT":
-            state = {
-                ...state,
-                result: state.result - action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            };
-            break;
-        default:
-            break;
-    }
-    return state;
-};
-
 const userReducer = (state = {
-    name: "Max",
-    age: 27
+  firstName: 'Sample',
+  lastName: 'User',
+  email: 'sample_user@gmail.com'
 }, action) => {
-    switch (action.type) {
-        case "SET_NAME":
-            state = {
-                ...state,
-                name: action.payload
-            };
-            break;
-        case "SET_AGE":
-            state = {
-                ...state,
-                age: action.payload
-            };
-            break;
-        default:
-            break;
-    }
-    return state;
+  switch (action.type) {
+    case 'UPDATE_PROFILE':
+      state = {
+        ...state,
+        firstName: action.payload.firstName,
+        lastName: action.payload.lastName,
+        email: action.payload.email,
+      };
+      break;
+
+    default:
+      break;
+  }
+  return state;
 };
 
 const loginReducer = (state = {
-    email: '',
-    password: ''
+  email: '',
+  password: ''
 }, action) => {
-    switch (action.type) {
-        case "LOGIN_REQUEST":
-            return {
-              ...state,
-            };
+  switch (action.type) {
+    case 'LOGIN_REQUEST':
+      return {
+        ...state,
+      };
 
-        case "LOGIN_RESPONSE":
-            return {
-              ...state,
-            };
+    case 'LOGIN_RESPONSE':
+      return {
+        ...state,
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default combineReducers({
-    math: mathReducer,
-    user: userReducer,
-    login: loginReducer,
+  user: userReducer,
+  login: loginReducer,
 });
