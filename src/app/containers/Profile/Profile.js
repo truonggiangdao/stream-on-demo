b217dcf223e1c127e51a822de1f3047c95962671
Giang Dao <giangdao@success-ss.com.vn>
2018-05-30 11:51:42 +0700

    bind profile to store

--- src/app/containers/Profile/Profile.js
+++ src/app/containers/Profile/Profile.js
@@ -1,40 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PageTitle from '@/components/PageTitle';
 import ProfileForm from './ProfileForm';
-// import api, {END_POINT_KEYS, parseAPIUrl} from '@/api';
 
 class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {},
-    };
-  }
-
-  componentDidMount() {
-    // const params = new URLSearchParams();
-    // params.append('email', 'admin@test.test');
-    // params.append('password', '123456');
-
-    // api.post(parseAPIUrl(END_POINT_KEYS.AUTH), params)
-    // .then(function (response) {
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-  }
-
   render() {
     return (
       <div>
         <PageTitle title="Profile"/>
         <div className="layout-container profile-container">
-          <ProfileForm user={this.state.user} />
+          <ProfileForm user={this.props.user} />
         </div>
       </div>
     );
   }
 };
 
-export default Profile;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps)(Profile);
