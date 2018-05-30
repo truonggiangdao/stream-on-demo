69256baf4c6a5c62ea0ec078a804c974d54a03fb
Giang Dao <giangdao@success-ss.com.vn>
2018-05-30 10:28:23 +0700

    update validatorHelper

---
+++ src/app/helpers/validator/validator.js
@@ -0,0 +1,120 @@
+import validator from 'validator';
+import INPUT_FIELDS from './fields.const';
+import MESSAGES from './messages.const';
+
+const validationResponse = {
+  valid: true,
+  errors: [],
+};
+
+const validate = (value, type, rules) => {
+  if (!type && !rules) {
+    if (value) {
+      return {
+        ...validationResponse,
+        valid: true,
+        errors: [],
+      };
+    }
+    return {
+      ...validationResponse,
+      valid: false,
+      errors: [
+        MESSAGES.REQUIRED,
+      ],
+    };
+  }
+
+  if (type === INPUT_FIELDS.EMAIL) {
+    return validateEmail(value);
+  }
+  if (type === INPUT_FIELDS.EMAIL) {
+    return validateEmail(value);
+  }
+};
+
+const validateRequired = value => {
+  if (!value) {
+    return {
+      ...validationResponse,
+      valid: false,
+      errors: [
+        MESSAGES.REQUIRED,
+      ],
+    };
+  }
+  return null;
+};
+
+const validateString = value => {
+  if (!value || value.constructor !== String) {
+    return {
+      ...validationResponse,
+      valid: false,
+      errors: [
+        MESSAGES.INVALID,
+      ],
+    };
+  }
+  return null;
+};
+
+const validateStringLength = (value, min, max) => {
+  if (min) {
+    if (validateRequired(value)) {
+      return validateRequired(value);
+    }
+  }
+  if (validator.isLength(value, {min, max})) {
+    return {
+      ...validationResponse,
+      valid: true,
+      errors: [],
+    };
+  }
+  return {
+    ...validationResponse,
+    valid: false,
+    errors: [
+      MESSAGES.INVALID,
+    ],
+  };
+};
+
+const validateEmail = value => {
+  if (validateRequired(value)) {
+    return validateRequired(value);
+  }
+  if (validator.isEmail(value)) {
+    return {
+      ...validationResponse,
+      valid: true,
+      errors: [],
+    };
+  }
+  return {
+    ...validationResponse,
+    valid: false,
+    errors: [
+      MESSAGES.INVALID,
+    ],
+  };
+};
+
+const validatePassword = value => {
+  if (validateRequired(value)) {
+    return validateRequired(value);
+  }
+  if (validateString(value)) {
+    return validateString(value);
+  }
+  return validateStringLength(value, 6, 15);
+};
+
+const validationHelper = {
+  validate,
+  validateEmail,
+  validatePassword,
+};
+
+export default validationHelper;
---
+++ src/app/helpers/validator/validator.test.js
@@ -0,0 +1,31 @@
+import validator from '../validator';
+import INPUT_FIELDS from './fields.const';
+
+it('Validate field errors', () => {
+  const result = validator.validate('abc', INPUT_FIELDS.EMAIL);
+  if (result.valid) {
+    throw new Error('Validate field errors not working. Expected valid: FALSE');
+  }
+  if (!result.errors || result.errors.constructor !== Array || result.errors.length === 0) {
+    throw new Error('Validate field errors not return errors as ARRAY');
+  }
+
+  result.errors.forEach(error => {
+    if (!error || error.constructor !== String) {
+      throw new Error('Validate field errors not return error messages as string');
+    }
+  });
+});
+
+it('Validate field valid', () => {
+  const result2 = validator.validate('abcd@gmail.com', INPUT_FIELDS.EMAIL);
+  if (!result2.valid) {
+    throw new Error('Validate field valid not working. Expected valid: TRUE');
+  }
+  if (!result2.errors || result2.errors.constructor !== Array) {
+    throw new Error('Validate field valid not return errors as ARRAY');
+  }
+  if (result2.errors.length > 0) {
+    throw new Error('Validate field valid should return errors as EMPTY ARRAY');
+  }
+});
