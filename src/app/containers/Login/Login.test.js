51afb1154253338f5537e694b1dc32d1e348462b
Giang Dao <giangdao@success-ss.com.vn>
2018-05-30 16:51:18 +0700

    update test

--- src/app/containers/Login/Login.test.js
+++ src/app/containers/Login/Login.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import Login from './Login';
+import store from '@/store';
 
 it('Login Component renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Login />, div);
+  ReactDOM.render((
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  ), div);
   ReactDOM.unmountComponentAtNode(div);
 });
