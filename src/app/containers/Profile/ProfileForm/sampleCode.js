a7ec2602ddf81e2cf4ce4e203bd13c0c11e4df20
Vi Ha <viha@success-ss.com.vn>
2018-06-04 17:57:49 +0700

    update file sample code

--- src/app/containers/Profile/ProfileForm/sampleCode.js
+++ src/app/containers/Profile/ProfileForm/sampleCode.js
@@ -2,16 +2,31 @@ let state = {
   firstName: 'Vi',
   lastName: 'Ha',
   errors: {
-    // ERROR
     firstName: '',
     lastName: '',
   },
 };
 
 const setState = (object) => {
-  state = Object.assign(state, object);
+  let arr = [];
+  for(let i = 0; i < Object.keys(object).length; i++) {
+    if (typeof Object.values(object)[i] === 'object') {
+      arr.push(Object.keys(object)[i]);
+    }
+  }
+  const getKey = (arr) => {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++) {
+      obj[arr[i]] = Object.assign({}, state[arr[i]], object[arr[i]]);
+    }
+    return obj;
+  };
+  // state = Object.assign(state, object, {
+  //   errors: Object.assign({}, state.errors, object.errors)
+  // });
+  state = Object.assign(state, object, getKey(arr));
 };
 
-setState({ firstName: 'Vi Ha', errors: { firstName: 'ERROR' } });
+setState({ firstName: 'Vi Ha', errors: { firstName: 'ERROR'} });
 
 export default state;
